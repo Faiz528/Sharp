ed1de04dfc051821b2706f49f86681eaa0db349e
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-03-22 22:14:52 +0530

    new style added

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var head =document.querySelector('#main-header');
 head.style.borderBottom="solid 4px black";
 var add = document.querySelector('.title');
 add.style.color="green";
+var lis = document.getElementsByClassName('list-group-item');
+lis[2].style.color="red";
+
+for(var i =0; i<lis.length ; i++)
+{
+    lis[i].style.fontWeight ="bold";
+}
